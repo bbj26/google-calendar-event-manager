c5037c48de21b29ea1d183f0c1ca43cb0fb81ae8
Branimir Corluka <brane.corluka@gmail.com>
2024-07-03 17:22:03 +0200

    add simple logs in controllers

--- server/controllers/calendar.controller.ts
+++ server/controllers/calendar.controller.ts
@@ -5,10 +5,15 @@ import {
   listEvents,
   updateEvent,
 } from "../services/calendar.service";
+import { Log } from "../database";
 
 export const getEvents = async (req: Request, res: Response) => {
   try {
     const events = await listEvents();
+    await Log.create({
+      method: "GET",
+      event_summary: "All events",
+    });
     res.json(events);
   } catch (error: any) {
     res.status(500).json({ error: error.message });
@@ -19,6 +24,10 @@ export const createCalendarEvent = async (req: Request, res: Response) => {
   try {
     const event = req.body;
     const createdEvent = await createEvent(event);
+    await Log.create({
+      method: "POST",
+      event_summary: createdEvent.summary,
+    });
     res.json(createdEvent);
   } catch (error: any) {
     res.status(500).json({ error: error.message });
@@ -30,6 +39,10 @@ export const updateCalendarEvent = async (req: Request, res: Response) => {
     const { eventId } = req.params;
     const { updatedEventBody } = req.body;
     const updatedEvent = await updateEvent(eventId, updatedEventBody);
+    await Log.create({
+      method: "PUT",
+      event_summary: updatedEvent.summary,
+    });
     res.json(updatedEvent);
   } catch (error: any) {
     res.status(500).json({ error: error.message });
@@ -40,6 +53,10 @@ export const deleteCalendarEvent = async (req: Request, res: Response) => {
   try {
     const { eventId } = req.params;
     const deleted = await deleteEvent(eventId);
+    await Log.create({
+      method: "DELETE",
+      event_summary: eventId,
+    });
     res.json(deleted);
   } catch (error: any) {
     res.status(500).json({ error: error.message });
